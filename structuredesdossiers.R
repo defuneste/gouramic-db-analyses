f9121d4e8c4ec3520cac873291ef4dd033d99519
defuneste <leroy.oli@gmail.com>
2020-02-21 17:26:38 +0100

    list des fil res de gouramix

---
+++ structuredesdossiers.R
@@ -0,0 +1,11 @@
+### premier script explo gouramic
+
+list.files("data", recursive = TRUE, pattern = "res.png$")
+
+list.files("data", recursive = TRUE, pattern = "0.res.png$")
+
+list.files("data", recursive = TRUE, pattern = "1.res.png$")
+
+list.files("data", recursive = TRUE, pattern = "2.res.png$")
+
+list.files("data", recursive = TRUE, pattern = "3.res.png$")
