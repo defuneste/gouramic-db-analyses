15c10469d1649b8ef45ad53d82077d4ca3ea313e
defuneste <leroy.oli@gmail.com>
2020-04-28 18:02:29 +0200

    geojson pour les NA à la main

--- Prétraitement_ETL/precision_geocodage.R
+++ Prétraitement_ETL/precision_geocodage.R
@@ -7,13 +7,16 @@ inst <- lapply(pkgs, library, character.only = TRUE)
 ## lecture des fichiers
 
 dist.dat <- read.csv("data/DistClean.csv", stringsAsFactors = FALSE) 
-geocodage_clbv2.shp <- sf::st_read("data/sortie_15_04.shp")
+geocodage_clbv2.shp <- sf::st_read("data/sortie_15_04.shp" , stringsAsFactors = FALSE)
 
 geocodage_clbv2.shp$date_start <- parse_date_time(geocodage_clbv2.shp$date_start, orders = c("my", "dmy"))
 geocodage_clbv2.shp$date_end_a <- parse_date_time(geocodage_clbv2.shp$date_end_a, orders = c("my", "dmy"))
 
-
 geocodage_evs.shp <- sf::st_read("data/geocodev2.geojson", stringsAsFactors = FALSE)
+
+geocodage_evs.shp$Date_birth <- as.Date(geocodage_evs.shp$Date_birth, origin = "1899-12-30")
+geocodage_evs.shp$Date_start <- as.Date(geocodage_evs.shp$Date_start, origin = "1899-12-30")
+geocodage_evs.shp$Date_end <- as.Date(geocodage_evs.shp$Date_start, origin = "1899-12-30")
 
 ## 1- Stats descriptives rapides =========================
 # un rapide boxplot
@@ -128,5 +131,23 @@ st_write(geocodage_clbv2_clean_dupli.shp, dsn = "data/clean_adresse_dupli.shp")
 # objectif ici est d'avoir les adresses à verifier 
 
 # on va commencer par les NA 
+
+# tout ce qui est dans geocodage EVS mais n'a pas de distance.
+NA_dist.dat <- geocodage_evs.shp[!geocodage_evs.shp$Id_cart %in%  dist.dat$ID_CARTO ,]
+
+# So on mets de coté ceux remplie à la mains 
+
+geocode_mains_Na <- NA_dist.dat[NA_dist.dat$source_loc == "main",] %>% 
+    select(Id_cart, Date_start, Date_end, Commune, Adresse, Code_postal, Info_sup, result_type) %>% 
+    st_transform(2154)
+
+geocodage_clb_mains <- geocodage_clbv2.shp[geocodage_clbv2.shp$ID_CARTO %in% geocode_mains_Na$Id_cart, ] %>% 
+    tidyr::unite("Info_sup", lieudit_p, compl_add_, pt_remarq_, sep = " ", na.rm = TRUE) %>% 
+    select(ID_CARTO, date_start, date_end_a, Commune, Adresse, CP, Info_sup, Match_addr, Loc_name)
+
+st_write(geocode_mains_Na, dsn = "data/geocode_mains_Na.geojson")
+st_write(geocodage_clb_mains , dsn = "data/geocodage_clb_mains.geojson")
+
+# ici c'est les differences dans les precisions
 
 filtre_geocode <- dist.dat[dist.dat$PreciBan != dist.dat$Preci_CLB,]
