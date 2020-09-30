836a4ca7e006b09a8db808b677f5d8ef9c9cca81
defuneste <leroy.oli@gmail.com>
2020-09-30 11:00:09 +0200

    dbi marche mais pas dbplyr

--- Prétraitement_ETL/exploration_db.R
+++ Prétraitement_ETL/exploration_db.R
@@ -3,7 +3,7 @@
 
 source("code.R") # ici j'ai mis codes/pwd/port/adresse c'est en .gitignore
 
-pkgs <-  c("DBI","RPostgreSQL", "dm",  "microbenchmark")
+pkgs <-  c("DBI","RPostgreSQL", "dm",  "microbenchmark", "sf")
 inst <- lapply(pkgs, library, character.only = TRUE)
 
 drv <- dbDriver("PostgreSQL")
@@ -25,11 +25,13 @@ dbExistsTable(con, "t_sujet")
 
 list_table <- c("t_sujet", "t_adresse", "p_t_adresse_interval", "t_interval_date")
 
-my_dm <- dm_from_src(con, list_table = c("t_sujet"), schema = c("gou"), learn_keys = F)
+my_dm <- dm_from_src(con, list_table = c("gou.t_sujet"), schema = "gou", learn_keys = F)
 my_dm
 
+st_read(con, query = "select * from gou.t_sujet limit 3;")
 
+tbl(con, in_schema("gou", "t_sujet"))
 
-tbl(con, from = "gou.t_adresse")
+tbl(con, from = "t_adresse")
 
 dbDisconnect(con)
