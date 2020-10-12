0b29dce4f381bb55508df817376fea6689480e76
defuneste <leroy.oli@gmail.com>
2020-10-12 11:25:54 +0200

    deplacement des images avant de faire le georef

---
+++ Prétraitement_ETL/deplacement_image.R
@@ -0,0 +1,30 @@
+### octobre 2020 
+## pour le georef on a besoin de toutes les images dans le repertoire image <- modifiable aussi au besoin dans le .py 
+# on va chercher les path des images qui nous interesse pour les mettres dans le bon repertoire
+
+# attention j'attrape par l'extension 
+# il y a des jpg et jp2
+
+ou_sont_les_images <- "data"
+ou_vont_les_images <- "/home/lo82302h/ortho/images/"
+
+liste_abouger <- c(
+                    list.files(ou_sont_les_images, recursive = TRUE, pattern = "jp2$"),
+                    list.files(ou_sont_les_images, recursive = TRUE, pattern = "jpg$"))
+
+
+# ici on prend le nom de l'image : principe c'est tout ce qui est après le C
+nom_image <- stringr::str_extract(liste_abouger, pattern = "(?<=C).*")
+# idem ne marche que si format XXXXXXX
+sujet <- substr(liste_abouger, 1,7)
+# plus souple prend ce qui est entre les / au besoin à utiliser pour sujet
+on_separe <- sapply(liste_abouger, function(x){ strsplit(x, "/")})
+adresse <- sapply(on_separe, function(x) {as.numeric(x[2])})
+
+# chemin avec nom de fichier
+un_chemin <- paste0(ou_vont_les_images, sujet, adresse, nom_image)
+# juste penser à rajouter le dir ou_sont_les_images
+
+file.copy(paste0(ou_sont_les_images, "/", liste_abouger), un_chemin)
+
+rm(nom_image, adresse, sujet, on_separe, un_chemin, ou_sont_les_images, ou_vont_les_images)
