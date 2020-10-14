bd930c6c2818c7d19b523d0299fe84652974c507
defuneste <leroy.oli@gmail.com>
2020-10-14 15:12:22 +0200

    prod de csv

--- Prétraitement_ETL/exploration_db.R
+++ Prétraitement_ETL/exploration_db.R
@@ -60,7 +60,7 @@ rm(sujet.dat, adresse.shp, p_t_adresse_interval.dat, adresse_sujet.shp, temporal
 temp <- adresse_sujet_temporal.shp[!is.na(adresse_sujet_temporal.shp$date_start),]
 
 temp  <- temp   %>% 
-    select(adresse_id,
+    select(adresse_id,adresse_clb, sujet_id,
            date_debut = date_start,
            date_fin = date_end) %>% 
     mutate(interval = interval(date_debut,date_fin)) %>% 
@@ -69,23 +69,11 @@ temp  <- temp   %>%
     st_transform(2154) %>% 
     st_buffer(2000) %>% 
     st_transform(4326) %>% 
-    dplyr::select(adresse_id, date_debut, date_fin, interval,  x , y, geometry) %>% 
+    dplyr::select(adresse_id,adresse_clb, sujet_id, date_debut, date_fin, interval,  x , y, geometry) %>% 
     arrange(adresse_id, date_debut)
 
 temp$date_debut <- year(temp$date_debut)
 temp$date_fin <- year(temp$date_fin)
-
-
-
-## un test léger sans la geometrie
-dat <- temp %>%
-            st_drop_geometry()
-
-dat %>%
-    nest(date_debut, date_fin) %>%
-    mutate(data = map(data, ~seq(unique(.x$date_debut), unique(.x$date_fin), 1))) %>%
-    unnest(data)
-
 
 temp_dupli <-  temp %>% 
     st_drop_geometry() %>% 
@@ -99,8 +87,25 @@ temp_dupli <-  temp %>%
     st_transform(2154) %>% 
     st_buffer(1500) %>% 
     st_transform(4326) %>% 
-    dplyr::select(adresse_id, date_y = data, x, y, geometry)
+    dplyr::select(adresse_id, adresse_clb, sujet_id, date_y = data, x, y, geometry)
 
+
+dir.create("data/csv")
+
+outlist <- list() # initialisation d'un liste
+longueur <- length(unique(temp_dupli$sujet_id)) # le nombre de fichier souhaité
+
+for(i in 1:longueur) { 
+    # on passe par une liste, c'est pas indispensable mais je voulais verifier un peu avant d'ecrire des fichier
+    outlist[[i]] <- temp_dupli %>%
+        filter(sujet_id == unique(temp_dupli$sujet_id)[i]) 
+    # on écrit des tas de fichiers 
+    st_write(outlist[[i]], dsn = paste0("data/csv/", unique(temp_dupli$sujet_id)[i], ".csv"),
+                sep = ";",
+                quote = FALSE,
+                row.names = FALSE,
+                col.names=TRUE)
+}
 
 # on peut se deconnecter
 dbDisconnect(con)
