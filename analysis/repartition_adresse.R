ecaf13bb165272adba2f63ff056690f8ef3281b5
defuneste <leroy.oli@gmail.com>
2020-10-21 16:40:49 +0200

    fonctions pour afficher les periodes des adresses par sujet, production de groupes pour calculee les trous

--- analysis/repartition_adresse.R
+++ analysis/repartition_adresse.R
@@ -10,6 +10,15 @@ source("Prétraitement_ETL/exploration_db.R")
 library(ggplot2)
 
 
+# on reprend le shape des communes avec l'info rural-urbain-peri
+communes.shp <- st_read("data/commune.shp")
+
+str(communes.shp)
+
+# rajout du type de commune par adresse !!! attention c'est le type de commune en 2019
+adresse_commune.shp <- st_join(st_transform(adresse_sujet_temporal.shp, 2154), st_transform(communes.shp[,c("TYPE_CO", "insee")], 2154))
+
+
 ##.###################################################################################33
 ## II. répartition temporelle ====
 ##.#################################################################################33
@@ -41,7 +50,44 @@ sujet.dat <- adresse_commune.shp %>%
                           date_max = max(date_end),
                           nbr_adresse = n())
 
-affiche_un_sujet("09_0431")
+bill <- affiche_un_sujet("15_0738")
+
+
+## fonction pour ploter les sujets avec leurs adresses groupées (ie jointive)
+# penser à renomer les variables intermediaires 
+
+plot_group_adresse <- function(liste_adresse){
+jim <- liste_adresse 
+jim$adresse_clb <- with(jim, reorder(adresse_clb, date_start)) 
+jim <- st_drop_geometry(jim)
+jim$adresse_jointives <- 1:nrow(jim)
+
+merge.indices <- lapply(2:nrow(jim), function(x) {
+    indices <- which(findInterval(jim$date_end[1:(x-1)], jim$date_start[x]) == 1 )
+    if (length(indices) > 0) indices <- c(indices, x) 
+    indices})
+
+for (i in 1:length(merge.indices)) {
+    if (length(merge.indices[[i]]) > 0) {
+        jim$adresse_jointives[merge.indices[[i]]] <- min(jim$adresse_jointives[merge.indices[[i]]])
+    }
+}
+
+ggplot(jim, aes(y = adresse_clb, x = date_start)) +
+    geom_segment(aes(x = date_start, y = adresse_clb, xend = date_end, yend = adresse_clb, col = as.factor(adresse_jointives), lwd = 1), data = jim) +
+    theme_bw()
+
+}
+
+plot_group_adresse(bill)
+
+
+
+as.duration(interval(first(jim$date_naissance), max(bob$end))) -
+as.duration(interval(first(test$date_naissance), max(test$date_end)))
+
+interval_total <- lubridate::interval(min(test$date_start), max(test$date_end))
+
 
 ##.###################################################################################33
 ## III. répartition spatiale / type d'espace ====
