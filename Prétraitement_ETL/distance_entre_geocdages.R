1c7ac26232b8d0153015c739071b37b9b89cf946
defuneste <leroy.oli@gmail.com>
2021-01-07 16:07:14 +0100

    fichier de verif

--- Prétraitement_ETL/distance_entre_geocdages.R
+++ Prétraitement_ETL/distance_entre_geocdages.R
@@ -5,13 +5,13 @@ library(sf)
 # rapport/prepa_publi.Rmd
 
 adresse <- sf::st_read("data/verif/distance.geojson")
-adresse_filtre <- adresse %>% 
-                sf::st_drop_geometry() %>% 
-                dplyr::select(ID_CARTO, preci_clb, preci_evs, distance)
+adresse_filtre <- sf::st_drop_geometry( adresse) 
+                
             
 # données deja geocodées
 
 geocoder_main <- sf::st_read("data/geocodage_main_total.geojson")
+st_write(geocoder_main, "data/verif/deja_geocoder.geojson")
 
 # on va chercher l'adresse 
 geocodage_clb.shp <- sf::st_read("data/sortie_15_04.shp" , stringsAsFactors = FALSE)
@@ -22,4 +22,14 @@ adresse_complement <- geocodage_clb.shp %>%
     tidyr::unite("Info_sup", lieudit_p, compl_add_, pt_remarq_, na.rm = TRUE)
 
 
-adresse_complement_distance <- dplyr::left_join(adresse_complement, )
+adresse_complement_distance <- dplyr::inner_join(adresse_complement, adresse_filtre, by = "ID_CARTO")
+
+adresse_complement_distance <- adresse_complement_distance[adresse_complement_distance$distance >= 5,]
+adresse_complement_distance$geocodage_main <- NA
+
+
+adresse_matthieu <- adresse_complement_distance[1:(length(adresse_complement_distance$ID_CARTO)/2),]
+st_write(adresse_matthieu, "data/verif/adresse_matthieu.geojson" )
+
+adresse_olivier <- adresse_complement_distance[(length(adresse_complement_distance$ID_CARTO)/2):length(adresse_complement_distance$ID_CARTO),]
+st_write(adresse_olivier, "data/verif/adresse_olivier.geojson")
