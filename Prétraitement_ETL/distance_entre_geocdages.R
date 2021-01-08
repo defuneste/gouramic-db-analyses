4f118196ca925ddcec0ee070e69785b3be54bc0a
defuneste <leroy.oli@gmail.com>
2021-01-08 10:59:40 +0100

    on vire les deja geocodées à la main

--- Prétraitement_ETL/distance_entre_geocdages.R
+++ Prétraitement_ETL/distance_entre_geocdages.R
@@ -6,12 +6,13 @@ library(sf)
 
 adresse <- sf::st_read("data/verif/distance.geojson")
 adresse_filtre <- sf::st_drop_geometry( adresse) 
-                
-            
+
+adresse_filtre$distance <-as.numeric(adresse_filtre$distance)
+
 # données deja geocodées
 
 geocoder_main <- sf::st_read("data/geocodage_main_total.geojson")
-st_write(geocoder_main, "data/verif/deja_geocoder.geojson")
+#st_write(geocoder_main, "data/verif/deja_geocoder.geojson")
 
 # on va chercher l'adresse 
 geocodage_clb.shp <- sf::st_read("data/sortie_15_04.shp" , stringsAsFactors = FALSE)
@@ -27,6 +28,9 @@ adresse_complement_distance <- dplyr::inner_join(adresse_complement, adresse_fil
 adresse_complement_distance <- adresse_complement_distance[adresse_complement_distance$distance >= 5,]
 adresse_complement_distance$geocodage_main <- NA
 
+# finalement on vire les  deja geocodés
+
+adresse_complement_distance <- adresse_complement_distance[!adresse_complement_distance$ID_CARTO %in% geocoder_main$adresse_id,]
 
 adresse_matthieu <- adresse_complement_distance[1:(length(adresse_complement_distance$ID_CARTO)/2),]
 st_write(adresse_matthieu, "data/verif/adresse_matthieu.geojson" )
