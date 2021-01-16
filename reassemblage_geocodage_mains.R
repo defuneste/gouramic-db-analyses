acbc9a84b923fce02306fa4b3ad77afc1e4bb1a2
defuneste <leroy.oli@gmail.com>
2021-01-16 16:37:49 +0100

    en rajoutant ce qui avait deja éte fait on est bon (il y a un peu de nettoyage à faire)

--- reassemblage_geocodage_mains.R
+++ reassemblage_geocodage_mains.R
@@ -32,7 +32,6 @@ dim(verif_ecart.shp)
 table(verif_ecart.shp$geocodage_main)
 
 ## ce qui avait deja éte goecodé à la main 
-
 adresse <- sf::st_read("data/verif/distance.geojson")
 adresse_filtre <- sf::st_drop_geometry(adresse) 
 adresse_filtre$distance <-as.numeric(adresse_filtre$distance)
@@ -40,10 +39,27 @@ rm(adresse)
 
 geocoder_main <- sf::st_read("data/geocodage_main_total.geojson")
 
-# on en a 93 qui avait déja été fait ...
+# on en a 93 qui avaient déja été fait ...
 geocoder_main[geocoder_main$adresse_id %in% verif_ecart.shp$ID_CARTO, ]
 
 deja_fait <- geocoder_main[geocoder_main$adresse_id %in% adresse_filtre$ID_CARTO ,]
 
 ce_qui_manque <- deja_fait[!deja_fait$adresse_id %in% verif_ecart.shp$ID_CARTO, ]
+
+ce_qui_manque.shp <- left_join(ce_qui_manque, adresse_filtre, by = c("adresse_id" = "ID_CARTO")) %>% 
+    dplyr::mutate(geocodage_main = "12",
+                  Adresse = NA) %>% 
+    dplyr::select(ID_CARTO = adresse_id,
+                  Commune = commune,
+                  CP = cp, 
+                  Adresse,
+                  Info_sup = info_sup,
+                  preci_clb = precision,
+                  preci_evs,
+                  distance, 
+                  geocodage_main,
+                  geocodeur = source_codage)
+
+verif_ecartv2.shp <- rbind(verif_ecart.shp, ce_qui_manque.shp)
+
 
