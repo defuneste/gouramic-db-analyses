be7693e29a774b8a25085e05f6fca1e8af00d732
Olivier Leroy <leroy.oli@gmail.com>
2021-03-30 17:15:51 +0200

    il faut remonter

--- Prétraitement_ETL/normalize_adressev2.R
+++ Prétraitement_ETL/normalize_adressev2.R
@@ -37,19 +37,28 @@ adresse_pre_cluster <- rbind(verif_ecartv3.shp[,a_garder], pas_verif_a_la_main, 
 
 rm(a_garder, adresse_complement, adresse_complement_distance, adresse_filtre, de_cotecorrigé.shp, pas_verif_a_la_main)
 
-
 ###### un cluster pour les gouverner tous =========================================================
 
 aggregat_filter_matthieu <- st_read("data/verif/aggregat_filter_matthieu(1).geojson")
+aggregat_filter_matthieu$source_codage <- "Main"
 aggregat_filter_olivier <- st_read("data/verif/aggregat_filter_olivier.geojson")
+aggregat_filter_olivier$source_codage <- "Main"
 
 aggregat_filter <- rbind(aggregat_filter_matthieu, aggregat_filter_olivier)
+
+rm(aggregat_filter_matthieu, aggregat_filter_olivier)
 
 # un peu de nettoyage on garde le cluster le plus large
 # on eneleve le cluster si idem == 0 ie n'est pas un cluster et du coup prend la valeur de 0
 cluster.shp <- aggregat_filter %>%
     mutate(cluster = ifelse(verif == 1, clust_100, 0) ) %>%
-    filter(cluster != 0) # on retire les non cluster
+    filter(cluster != 0) # on retire les non clusters
+
+pas_cluster.shp <-  aggregat_filter %>%
+    mutate(cluster = ifelse(verif == 1, clust_100, 0) ) %>%
+    filter(cluster == 0) # on garde les clusters
+
+rm(aggregat_filter)
 
 # il faut changer la loc des points formant le clusters par le point au milieu
 # comment definit-on le milieu ?
@@ -63,6 +72,7 @@ centre_cluster <- cluster.shp %>%
     right_join(cluster.shp, by = "cluster") %>%
     ungroup() %>%
     st_as_sf(sf_column_name = "geometry")
+
 
 table(centre_cluster$count)
 
@@ -88,7 +98,7 @@ centre_cluster_ligne <- aggregate(
 # match est utilise pour produire un vecteur d'indexation attribuant on va attribuer le point
 centre_cluster$geometry[centre_cluster$count == 2] <- st_sfc(centre_cluster_ligne$geometry)[match(centre_cluster$clust_100[centre_cluster$count == 2],  centre_cluster_ligne$Group.1)]
     
-    st_write(centre_cluster, "data/verif/verif_cluster.geojson")
+#st_write(centre_cluster, "data/verif/verif_cluster.geojson")
 
 # on prepare pour un rajout
 transit <- data.frame(
@@ -99,36 +109,34 @@ names(transit) <- c("clust_100", "addresse_passage")
 
 centre_cluster <- centre_cluster %>% left_join(transit,  by = c("clust_100" = "clust_100"))
 
-# # un bout de la futur table de passage
-# transit_passage <- centre_cluster %>% 
-#     st_drop_geometry() %>% 
-#     dplyr::select(addresse_passage, adresse_clb)  #%>%
-# #dplyr::mutate(adresse_id = 1:length(addresse_passage))
-# 
-# names(transit_passage) <- c("adresse_id", "adresse_passage")
-# 
-# length(unique(geocodage_adresse.shp$adresse_id))
-# 
-# centre_cluster_clean <- centre_cluster %>% 
-#     group_by(addresse_id) %>% 
+rm(transit)
+
+# un bout de la futur table de passage
+transit_passage <- centre_cluster %>%
+    st_drop_geometry() %>%
+    dplyr::select(addresse_passage, ID_CARTO)  
+
+# centre_cluster_clean <- centre_cluster %>%
+#     group_by(adresse_passage) %>%
 #     summarize(adresse_clb = first(adresse_id),
 #               sujet_id = first(sujet_id),
-#               precision = first(precision),
-#               source_codage = first(source_codage)) 
+#               precision = first(preci_clb),
+#               source_codage = first(source_codage))
 # 
 # # il faut retirer les clusters et preparer le jeux de données
+# puis les rajouter et ceux non definit comme cluster
 # # c'est un peu lourd en computation pour ce que cela fait ...
 # # il y a l'ajout puis la mise en forme
-# table_adresse.shp <- geocodage_adresse.shp[!geocodage_adresse.shp$adresse_id %in% centre_cluster$adresse_id,] %>% 
-#     select(-c(date_start, date_end, commune, adresse, cp, info_sup,  nb_cluster, nb_bigcluster)) %>% 
-#     bind_rows(centre_cluster_clean) %>% 
-#     group_by(adresse_id) %>% # c'est pas ultra propre
-#     summarize(sujet_id = first(sujet_id),
-#               precision = first(precision),
-#               source_codage = first(source_codage)) %>% 
-#     dplyr::mutate(adresse_clb = adresse_id) %>% 
-#     dplyr::mutate(adresse_id = 1:length(adresse_id))  %>% 
-#     dplyr::select(adresse_id, sujet_id, adresse_clb, precision, source_codage)
+table_adresse.shp <- adresse_pre_cluster[!adresse_pre_cluster$ID_CARTO %in% centre_cluster$ID_CARTO,] %>%
+    select(-c(date_start, date_end, commune, adresse, cp, info_sup,  nb_cluster, nb_bigcluster)) %>%
+    bind_rows(centre_cluster_clean) %>%
+    group_by(adresse_id) %>% # c'est pas ultra propre
+    summarize(sujet_id = first(sujet_id),
+              precision = first(precision),
+              source_codage = first(source_codage)) %>%
+    dplyr::mutate(adresse_clb = adresse_id) %>%
+    dplyr::mutate(adresse_id = 1:length(adresse_id))  %>%
+    dplyr::select(adresse_id, sujet_id, adresse_clb, precision, source_codage)
 # 
 # # il y a des id de sujet avec  des fautes de frappes à corriger
 # # oui j'ai verifier 08_006X
